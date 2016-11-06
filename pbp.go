2983bd16fe0dabf4d7ac70c83250cfb60ae46a6c
Andres Villagra <andres_ve@icloud.com>
2016-11-06 00:46:07 -0300

    Static file serving: done

--- pbp.go
+++ pbp.go
@@ -3,6 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
-func main(){}
+func main() {
+	http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
+		fileName := r.URL.String()[7:]
+		file, err := os.Open("static/" + fileName)
+		fileText := make([]byte, 2048, 2048)
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		file.Read(fileText)
+		w.Write(fileText)
+	})
+
+	fmt.Print(http.ListenAndServe(":8080", nil))
+}
